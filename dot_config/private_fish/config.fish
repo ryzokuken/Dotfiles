95d4bac88f2406521e47c0a8f1006b07a6818af1
Ujjwal Sharma <ryzokuken@disroot.org>
2020-03-19 13:49:00 +0530

    fish: one editor to rule 'em all

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,10 @@
-alias vim nvim
 alias c chezmoi
 alias login "~/.caa/caa --stop && ~/.caa/caa --daemon"
 set -x GPG_TTY (tty)
+
+# One editor to rule them all
+set EDITOR nvim
+alias vi="nvim"
+alias vim="nvim"
+
+thefuck --alias | source
