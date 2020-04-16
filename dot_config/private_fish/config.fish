23e6f465d51641ef77d57826099e2b4f36dd3137
Ujjwal Sharma <ryzokuken@disroot.org>
2020-04-16 23:38:53 +0530

    fish: set alacritty and customize cat default

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,11 +1,12 @@
 set -gx GPG_TTY (tty)
 set -gx EDITOR nvim
+set -gx TERMINAL alacritty
 
 alias c chezmoi
 alias vi nvim
 alias vim nvim
 alias ls lsd
-alias cat bat
+alias cat "bat --style=plain"
 
 thefuck --alias | source
 
